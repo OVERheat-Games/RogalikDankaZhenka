a0fd45121f1c81e3975b614ea1872f98c02b38e6
dan52rusik <85310826+dan52rusik@users.noreply.github.com>
2023-11-09 00:19:37 +0300

    коллайдеры стен и дверей гуд
    я лох

--- Assets/Scripts/Map/Wall Colider.cs
+++ Assets/Scripts/Map/Wall Colider.cs
@@ -2,22 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallColider : MonoBehaviour
+public class WallCollider : MonoBehaviour
 {
-    public GameObject WithDoor;
-    public GameObject WithoutDoor;
+    public GameObject TopWithDoor;
+    public GameObject TopWithoutDoor;
+    public GameObject BottomWithDoor;
+    public GameObject BottomWithoutDoor;
+    public GameObject LeftWithDoor;
+    public GameObject LeftWithoutDoor;
+    public GameObject RightWithDoor;
+    public GameObject RightWithoutDoor;
 
     void Update()
     {
-       if(GameObject.Find("TopDoor"))
-       {
-            WithDoor.SetActive(true); 
-            WithoutDoor.SetActive(false);
-       }
-       else
-       {
-            WithDoor.SetActive(false); 
-            WithoutDoor.SetActive(true);
-       }
+        if (GameObject.Find("TopDoor"))
+        {
+            TopWithDoor.SetActive(true);
+            TopWithoutDoor.SetActive(false);
+        }
+        else
+        {
+            TopWithDoor.SetActive(false);
+            TopWithoutDoor.SetActive(true);
+        }
+
+        if (GameObject.Find("BottomDoor"))
+        {
+            BottomWithDoor.SetActive(true);
+            BottomWithoutDoor.SetActive(false);
+        }
+        else
+        {
+            BottomWithDoor.SetActive(false);
+            BottomWithoutDoor.SetActive(true);
+        }
+
+        if (GameObject.Find("LeftDoor"))
+        {
+            LeftWithDoor.SetActive(true);
+            LeftWithoutDoor.SetActive(false);
+        }
+        else
+        {
+            LeftWithDoor.SetActive(false);
+            LeftWithoutDoor.SetActive(true);
+        }
+
+        if (GameObject.Find("RightDoor"))
+        {
+            RightWithDoor.SetActive(true);
+            RightWithoutDoor.SetActive(false);
+        }
+        else
+        {
+            RightWithDoor.SetActive(false);
+            RightWithoutDoor.SetActive(true);
+        }
     }
 }
+
